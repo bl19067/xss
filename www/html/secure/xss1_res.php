9d9282459915a010e4481f1ffc7c9b11f9643004
bl19067 <bl19067@cc.it-hiroshima.ac.jp>
2023-02-08 19:05:11 +0900

    エスケープ処理外した

--- www/html/secure/xss1_res.php
+++ www/html/secure/xss1_res.php
@@ -8,7 +8,7 @@
 	<body>
 		<?php 
 		#脆弱だよー
-		echo 'hoge!!!!!!!!!!!! ' .htmlspecialchars($_POST['data']);
+		echo 'hoge!!!!!!!!!!!! ' $_POST['data'];
 		?>
 	</br>
 	<div style="padding: 15px;"></div>
