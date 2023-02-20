ee415133a348aa9bea8e72dec8526b3f19f8814a
bl19067 <bl19067@cc.it-hiroshima.ac.jp>
2023-02-20 21:44:59 +0900

    アドレス固定

--- www/html/secure/xss2_res.php
+++ www/html/secure/xss2_res.php
@@ -14,7 +14,7 @@ $name = $_POST['name'];
 $detail  = $_POST['detail'];
 
     try{
-      $pdo = new PDO('mysql:dbname=testdb;host=192.168.192.2',$user,$pass);
+      $pdo = new PDO('mysql:dbname=testdb;host=192.168.0.30',$user,$pass);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }catch (PDOException $e) 
